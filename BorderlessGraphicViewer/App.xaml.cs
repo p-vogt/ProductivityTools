580c221c37e28a12707d7f08e411d5ac91fc139e
p-vogt <patrick.vogt@fh-bielefeld.de>
2017-06-14 11:13:16 +0200

    drawing function added

--- BorderlessGraphicViewer/App.xaml.cs
+++ BorderlessGraphicViewer/App.xaml.cs
@@ -15,21 +15,16 @@ namespace BorderlessGraphicViewer
     {
         void App_Startup(object sender, StartupEventArgs e)
         {
-            // Application is running
-            // Process command line args
-            bool startMinimized = false;
-            for (int i = 0; i != e.Args.Length; ++i)
-            {
-                
-            }
-
-            // Create main application window, starting minimized if specified
             MainWindow mainWindow = new MainWindow(e.Args);
-            if (startMinimized)
+
+            try
             {
-                mainWindow.WindowState = WindowState.Minimized;
+                mainWindow.Show();
             }
-            mainWindow.Show();
+            catch (Exception)
+            {
+            }
+          
         }
     }
 }
--- BorderlessGraphicViewer/MainWindow.xaml.cs
+++ BorderlessGraphicViewer/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +24,8 @@ namespace BorderlessGraphicViewer
     public partial class MainWindow : Window
     {
         BitmapImage image;
+        //initial image (without drawings)
+        BitmapImage imageInit;
         public MainWindow(String[] args)
         {
             InitializeComponent();
@@ -32,14 +36,23 @@ namespace BorderlessGraphicViewer
             }
             else
             {
-                filename = @"C:\Users\Patrick\Documents\Visual Studio 2015\Projects\Programmsammlung\BorderlessGraphicViewer\debug.png";
+#if DEBUG
+                String appDirPath = System.Reflection.Assembly.GetEntryAssembly().Location;
+                String projectPath = Directory.GetParent(appDirPath).Parent.Parent.FullName;
 
+                
+                
+                filename = projectPath + @"\debug.png";
+#endif
             }
             try
             {
 
-                image = new BitmapImage(new Uri(@filename, UriKind.Absolute));
-                img.Source = image;
+                imageInit = new BitmapImage(new Uri(@filename, UriKind.Absolute));
+                image = imageInit;
+                img.Source = imageInit;
+
+
             }
             catch (Exception)
             {
@@ -60,6 +73,9 @@ namespace BorderlessGraphicViewer
             }
             else if(Keyboard.IsKeyDown(Key.F5))
             {
+                image = imageInit;
+                img.Source = imageInit;
+
                 FitWindowSize();
             }
         }
@@ -76,6 +92,79 @@ namespace BorderlessGraphicViewer
 
             Height = image.Height + heightDifference;
             Width = image.Width + widthDifference;
+        }
+
+        System.Windows.Point lastMousePos = new System.Windows.Point(-1.0,-1.0);
+        private void img_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                if(lastMousePos.X == -1.0)
+                {
+                    lastMousePos = Mouse.GetPosition(this);
+                }
+                DrawingVisual dv = new DrawingVisual();
+                using (DrawingContext dc = dv.RenderOpen())
+                {
+                    var converter = new BrushConverter();
+                    var brush = (System.Windows.Media.Brush)converter.ConvertFromString("#FF0000");
+                    var pen = new System.Windows.Media.Pen();
+                    pen.Brush = brush;
+                    pen.Thickness = 2;
+                    
+
+                    System.Windows.Point newMousePos = Mouse.GetPosition(this);
+
+                    dc.DrawImage(image, new Rect(0, 0, image.PixelWidth, image.PixelHeight));
+                    
+                    dc.DrawLine(pen, lastMousePos,newMousePos);
+                    lastMousePos = newMousePos;
+                }
+
+                RenderTargetBitmap rtb = new RenderTargetBitmap(image.PixelWidth, image.PixelHeight, 96, 96, PixelFormats.Pbgra32);
+                rtb.Render(dv);
+
+                var bitmapEncoder = new PngBitmapEncoder();
+
+                bitmapEncoder.Frames.Add(BitmapFrame.Create(rtb));
+
+                using (var stream = new MemoryStream())
+                {
+                    bitmapEncoder.Save(stream);
+                    stream.Seek(0, SeekOrigin.Begin);
+                    BitmapImage newImage = new BitmapImage();
+                    newImage.BeginInit();
+                    newImage.CacheOption = BitmapCacheOption.OnLoad;
+                    newImage.StreamSource = stream;
+                    newImage.EndInit();
+                    image = newImage;
+                    img.Source = image;
+                }
+
+            }
+
+
+        }
+
+        private Bitmap ConvertToBitmap(BitmapSource target)
+        {
+            Bitmap bitmap;
+
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(target));
+                enc.Save(outStream);
+                bitmap = new Bitmap(outStream);
+            }
+
+            return bitmap;
+        }
+
+
+        private void img_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            lastMousePos = new System.Windows.Point(-1.0, -1.0);
         }
     }
 }
