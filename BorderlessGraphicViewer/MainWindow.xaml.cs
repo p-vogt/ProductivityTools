166030519e64b1e92a8b3c1d4c517b33369c3022
p-vogt <patrick.vogt@fh-bielefeld.de>
2017-08-12 20:21:26 +0200

    add undo drawing with right click

--- BorderlessGraphicViewer/MainWindow.xaml.cs
+++ BorderlessGraphicViewer/MainWindow.xaml.cs
@@ -20,7 +20,8 @@ namespace BorderlessGraphicViewer
         private const string INTERNAL_CALL_FLAG = "-internalCall";
         private bool newPictureOnStack = false;
         private Stack<BitmapImage> imageStack = new Stack<BitmapImage>();
-
+        private bool isDrawingCanceled;
+        private bool isCurrentlyDrawing;
         private BitmapImage image;
         //initial image (without drawings)
         private BitmapImage imageInit;
@@ -74,13 +75,13 @@ namespace BorderlessGraphicViewer
         {
             if ((Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))
             && Keyboard.IsKeyDown(Key.C))
-            { 
-                if(image != null)
+            {
+                if (image != null)
                 {
                     Clipboard.SetImage(image);
                 }
             }
-            else if(Keyboard.IsKeyDown(Key.F5))
+            else if (Keyboard.IsKeyDown(Key.F5))
             {
                 image = imageInit;
                 img.Source = imageInit;
@@ -94,23 +95,28 @@ namespace BorderlessGraphicViewer
             else if ((Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))
             && Keyboard.IsKeyDown(Key.Z))
             {
-                // first time pressing ctrl+z after new pic
-                if(newPictureOnStack)
-                {
-                    imageStack.Pop();
-                    newPictureOnStack = false;
-                }
-                if(imageStack.Count > 1)
-                {
-                    image = imageStack.Pop();
- 
-                }
-                else
-                {
-                    image = imageStack.Peek();
-                }
-                img.Source = image;
+                UndoDrawing();
             }
+        }
+
+        private void UndoDrawing()
+        {
+            // first time pressing ctrl+z after new pic
+            if (newPictureOnStack)
+            {
+                imageStack.Pop();
+                newPictureOnStack = false;
+            }
+            if (imageStack.Count > 1)
+            {
+                image = imageStack.Pop();
+
+            }
+            else
+            {
+                image = imageStack.Peek();
+            }
+            img.Source = image;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -127,67 +133,75 @@ namespace BorderlessGraphicViewer
             Width = image.Width + widthDifference;
         }
 
-        System.Windows.Point lastMousePos = new System.Windows.Point(-1.0,-1.0);
+        System.Windows.Point lastMousePos = new System.Windows.Point(-1.0, -1.0);
         private void img_MouseMove(object sender, MouseEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
             {
-                if(lastMousePos.X == -1.0)
+                if (!isDrawingCanceled)
                 {
-                    lastMousePos = Mouse.GetPosition(this);
-                }
-                DrawingVisual dv = new DrawingVisual();
-                using (DrawingContext dc = dv.RenderOpen())
-                {
-                    try
-                    {
-                        var converter = new BrushConverter();
-                        var brush = (System.Windows.Media.Brush)converter.ConvertFromString("#FF0000");
-                        var pen = new System.Windows.Media.Pen();
-                        pen.Brush = brush;
-                        pen.Thickness = 2;
-
-                        System.Windows.Point newMousePos = Mouse.GetPosition(this);
-
-                        dc.DrawImage(image, new Rect(0, 0, image.PixelWidth, image.PixelHeight));
-
-                        System.Windows.Point relativeP1 = new System.Windows.Point(image.Width * (lastMousePos.X / img.ActualWidth), (image.Height * lastMousePos.Y / img.ActualHeight));
-                        System.Windows.Point relativeP2 = new System.Windows.Point(image.Width * (newMousePos.X / img.ActualWidth), (image.Height * newMousePos.Y / img.ActualHeight));
-                        dc.DrawLine(pen, relativeP1, relativeP2);
-
-                        lastMousePos = newMousePos;
-                    }
-                    catch (Exception)
-                    {
-                    }
+                    DrawWithMouseOnImage();
                 }
 
-                RenderTargetBitmap rtb = new RenderTargetBitmap(image.PixelWidth, image.PixelHeight, 96, 96, PixelFormats.Pbgra32);
-                rtb.Render(dv);
+            }
+        }
 
-                var bitmapEncoder = new PngBitmapEncoder();
-
-                bitmapEncoder.Frames.Add(BitmapFrame.Create(rtb));
-
-                using (var stream = new MemoryStream())
+        private void DrawWithMouseOnImage()
+        {
+            if (lastMousePos.X == -1.0)
+            {
+                lastMousePos = Mouse.GetPosition(this);
+            }
+            DrawingVisual dv = new DrawingVisual();
+            using (DrawingContext dc = dv.RenderOpen())
+            {
+                try
                 {
-                    bitmapEncoder.Save(stream);
-                    stream.Seek(0, SeekOrigin.Begin);
-                    BitmapImage newImage = new BitmapImage();
-                    newImage.BeginInit();
-                    newImage.CacheOption = BitmapCacheOption.OnLoad;
-                    newImage.StreamSource = stream;
-                    newImage.EndInit();
-                    image = newImage;
-                    img.Source = image;
+                    var converter = new BrushConverter();
+                    var brush = (System.Windows.Media.Brush)converter.ConvertFromString("#FF0000");
+                    var pen = new System.Windows.Media.Pen();
+                    pen.Brush = brush;
+                    pen.Thickness = 2;
+
+                    System.Windows.Point newMousePos = Mouse.GetPosition(this);
+
+                    dc.DrawImage(image, new Rect(0, 0, image.PixelWidth, image.PixelHeight));
+
+                    System.Windows.Point relativeP1 = new System.Windows.Point(image.Width * (lastMousePos.X / img.ActualWidth), (image.Height * lastMousePos.Y / img.ActualHeight));
+                    System.Windows.Point relativeP2 = new System.Windows.Point(image.Width * (newMousePos.X / img.ActualWidth), (image.Height * newMousePos.Y / img.ActualHeight));
+                    dc.DrawLine(pen, relativeP1, relativeP2);
+
+                    lastMousePos = newMousePos;
                 }
+                catch (Exception)
+                {
+                }
+            }
+
+            RenderTargetBitmap rtb = new RenderTargetBitmap(image.PixelWidth, image.PixelHeight, 96, 96, PixelFormats.Pbgra32);
+            rtb.Render(dv);
+
+            var bitmapEncoder = new PngBitmapEncoder();
+
+            bitmapEncoder.Frames.Add(BitmapFrame.Create(rtb));
+
+            using (var stream = new MemoryStream())
+            {
+                bitmapEncoder.Save(stream);
+                stream.Seek(0, SeekOrigin.Begin);
+                BitmapImage newImage = new BitmapImage();
+                newImage.BeginInit();
+                newImage.CacheOption = BitmapCacheOption.OnLoad;
+                newImage.StreamSource = stream;
+                newImage.EndInit();
+                image = newImage;
+                img.Source = image;
             }
         }
 
         private Bitmap ConvertToBitmap(BitmapSource target)
         {
             Bitmap bitmap;
-
             using (MemoryStream outStream = new MemoryStream())
             {
                 BitmapEncoder enc = new BmpBitmapEncoder();
@@ -199,19 +213,46 @@ namespace BorderlessGraphicViewer
             return bitmap;
         }
 
-
         private void img_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
-            lastMousePos = new System.Windows.Point(-1.0, -1.0);
+            if (e.RightButton == MouseButtonState.Pressed)
+            {
+                if (isCurrentlyDrawing)
+                {
+                    isDrawingCanceled = true;
+                }
+                if (e.LeftButton != MouseButtonState.Pressed)
+                {
+                    img.ContextMenu.IsOpen = true;
+                }
+            }
+            else
+            {
+                isDrawingCanceled = false;
+                isCurrentlyDrawing = true;
+                lastMousePos = new System.Windows.Point(-1.0, -1.0);
+            }
         }
 
+        private bool __ignoreOneMouseUp; // see @ usage
         private void img_MouseUp(object sender, MouseButtonEventArgs e)
         {
             imageStack.Push(image);
             newPictureOnStack = true;
+            if (isDrawingCanceled)
+            {
+                // little hack to prevent 2 img to be removed from the stack when releasing left and right button
+                // this event gets called twice...
+                __ignoreOneMouseUp = !__ignoreOneMouseUp;
+                if (__ignoreOneMouseUp)
+                {
+                    UndoDrawing();
+                }
+            }
+            isCurrentlyDrawing = false;
         }
 
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        private void MenuItemOpenWithPaint_Click(object sender, RoutedEventArgs e)
         {
             string tmpPicName = "temp.png";
 
@@ -261,7 +302,7 @@ namespace BorderlessGraphicViewer
                 // Save document
                 this.SaveAsImageAsPng(dlg.FileName);
             }
-        }        
+        }
     }
 
 }
